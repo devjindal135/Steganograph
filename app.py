9dcf1cbda14fb5515a0c971ccd787f981e64454f
devjindal135 <devjindal135@gmail.com>
2025-02-26 18:49:45 +0530

    Steganography

--- app.py
+++ app.py
@@ -8,45 +8,58 @@ UPLOAD_FOLDER = 'static/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Encryption function
-def encrypt_image(img_path, message):
+# XOR encryption function
+def xor_encrypt(message, password):
+    return [ord(c) ^ ord(password[i % len(password)]) for i, c in enumerate(message)]
+
+# XOR decryption function
+def xor_decrypt(encrypted_values, password):
+    return "".join(chr(c ^ ord(password[i % len(password)])) for i, c in enumerate(encrypted_values))
+
+# Encrypt message into image
+def encrypt_image(img_path, message, password):
     img = cv2.imread(img_path)
     height, width, _ = img.shape
-    max_length = height * width * 3  # Maximum characters we can store
+    max_length = height * width * 3  # Max characters we can store
 
-    if len(message) > max_length:
-        return None  # Message is too long
+    encrypted_message = xor_encrypt(message + "##END##", password)
 
-    message += "##END##"  # To mark end of the message
+    if len(encrypted_message) > max_length:
+        return None  # Message too long
+
     m, n, z = 0, 0, 0
-
-    for char in message:
-        img[n, m, z] = ord(char)
+    for value in encrypted_message:
+        img[n, m, z] = value  # Store encrypted ASCII value
         n = (n + 1) % height
         m = (m + 1) % width
         z = (z + 1) % 3
-    
+
     encrypted_path = os.path.join(UPLOAD_FOLDER, 'encrypted.png')
     cv2.imwrite(encrypted_path, img)
     return encrypted_path
 
-# Decryption function
-def decrypt_image(img_path):
+# Decrypt message from image
+def decrypt_image(img_path, password):
     img = cv2.imread(img_path)
     height, width, _ = img.shape
-    message = ""
-    m, n, z = 0, 0, 0
+    encrypted_values = []
+    max_length = height * width * 3  # Prevent infinite loop
 
-    while True:
-        char = chr(img[n, m, z])
-        if message.endswith("##END##"):
-            break
-        message += char
+    m, n, z = 0, 0, 0
+    for _ in range(max_length):  # Limit extraction to prevent infinite loop
+        value = img[n, m, z]
+        encrypted_values.append(value)  # Retrieve stored ASCII value
+
         n = (n + 1) % height
         m = (m + 1) % width
         z = (z + 1) % 3
 
-    return message.replace("##END##", "")
+        # Attempt to decrypt and check for ending
+        decrypted_message = xor_decrypt(encrypted_values, password)
+        if decrypted_message.endswith("##END##"):
+            return decrypted_message.replace("##END##", "")
+
+    return None  # If no valid message is found, assume incorrect password
 
 @app.route("/")
 def index():
@@ -61,7 +74,7 @@ def encrypt():
     img_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(img_path)
 
-    encrypted_img_path = encrypt_image(img_path, message)
+    encrypted_img_path = encrypt_image(img_path, message, password)
 
     if encrypted_img_path:
         return jsonify({"success": True, "image_url": f"/{encrypted_img_path}"})
@@ -75,7 +88,11 @@ def decrypt():
     img_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(img_path)
 
-    message = decrypt_image(img_path)
+    message = decrypt_image(img_path, password)
+
+    if message is None:
+        return jsonify({"success": True, "error": "Incorrect password!"})
+
     return jsonify({"success": True, "message": message})
 
 if __name__ == "__main__":
